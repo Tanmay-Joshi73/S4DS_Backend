66996ecfde8f06367dfdc2d6e94df62e24216747
Tanmay Joshi <tanmayjoshi072@gmail.com>
2025-01-05 22:24:52 +0530

    Routs handelling added

--- Routes/route.js
+++ Routes/route.js
@@ -3,4 +3,7 @@ const {Show,FormHandelling}=require("../Controllers/Controller")
 const routes=express.Router()
 routes.get('/Get',Show)
 routes.post('/FormData',FormHandelling)
+routes.get('*',(req,res)=>{
+    res.redirect('/App/Get')
+})
 module.exports=routes;
--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose
   .catch((err) => console.error(err));
 
 app.use('/App',UserRoutes)
+
 app.listen(8000,'0.0.0.0',()=>{
     console.log("Listening from the server")
 })
